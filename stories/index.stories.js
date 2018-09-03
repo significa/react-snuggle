c351ff1d6828d521df59cba329832e14a20d0640
Danilo Woznica <danilowoz@gmail.com>
2018-09-03 14:25:01 +0100

    feat(Plugin): Reveal scroll

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,33 +1,16 @@
-import React from "react"
+import React, { Component } from "react"
 import { storiesOf } from "@storybook/react"
 import { withKnobs, text, boolean, number } from "@storybook/addon-knobs"
-import styled from "styled-components"
 
 import "./style.css"
 import Masonry from "../src"
 
-const ItemStyled = styled.div`
-  color: #fff;
-  border-bottom: 1px solid #fff;
-`
-
-const Image = ({ index }) => (
-  <img className="image" src={`https://picsum.photos/100${index % 10}/600`} />
-)
-
-const listElements = image =>
-  Array(30)
-    .fill(" ")
-    .map((item, index) => (
-      <div key={index}>
-        {image ? <Image index={index} /> : null}
-        <p>
-          {index % 2 === 0
-            ? "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce facilisis fringilla laoreet. Mauris mattis enim ut felis consectetur, vitae lacinia enim auctor. Aenean vitae fermentum odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dictum non orci ut dignissim. Fusce fermentum felis aliquam, mattis nibh ut, faucibus leo. Sed lectus libero, volutpat at eros quis, venenatis tempus neque. Nulla vel faucibus orci."
-            : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce facilisis fringilla laoreet. Mauris mattis enim ut felis consectetur,"}
-        </p>
-      </div>
-    ))
+import {
+  ItemStyled,
+  listElements,
+  OnUpdateGrid,
+  RevealAnimation
+} from "./parts"
 
 storiesOf("Masonry", module)
   .addDecorator(withKnobs)
@@ -44,6 +27,15 @@ storiesOf("Masonry", module)
     </div>
   ))
 
+  .add("no style", () => (
+    <div className="wrap">
+      <Masonry>{listElements()}</Masonry>
+    </div>
+  ))
+
+  .add("on update grid", () => <OnUpdateGrid />)
+
+storiesOf("Options", module)
   .add("custom container (<ul />)", () => (
     <div className="wrap">
       <Masonry
@@ -60,17 +52,6 @@ storiesOf("Masonry", module)
       <Masonry
         container={<ul style={{ background: "#5050FF" }} />}
         item={<li style={{ background: "#fff" }} />}
-      >
-        {listElements(true)}
-      </Masonry>
-    </div>
-  ))
-
-  .add("with styled-component", () => (
-    <div className="wrap">
-      <Masonry
-        container={<ul style={{ background: "#5050FF" }} />}
-        item={<ItemStyled />}
       >
         {listElements(true)}
       </Masonry>
@@ -98,8 +79,16 @@ storiesOf("Masonry", module)
     </div>
   ))
 
-  .add("no style", () => (
+storiesOf("Third parts", module)
+  .add("with styled-component", () => (
     <div className="wrap">
-      <Masonry>{listElements()}</Masonry>
+      <Masonry
+        container={<ul style={{ background: "#5050FF" }} />}
+        item={<ItemStyled />}
+      >
+        {listElements(true)}
+      </Masonry>
     </div>
   ))
+
+  .add("with scroll reveal", () => <RevealAnimation />)
