576f27a3b6a34af5c0bed296e5a92112c1e6ef9e
Danilo Woznica <danilowoz@gmail.com>
2019-01-31 19:05:26 +0000

    refactor(Grid): Change style approach (#19)

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,81 +1,86 @@
-import React from "react"
-import { storiesOf } from "@storybook/react"
-import { withKnobs, text, boolean, number } from "@storybook/addon-knobs"
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs'
 
-import "./style.css"
-import Snuggle from "../src"
+import './style.css'
+import Snuggle from '../src'
 
 import {
   ItemStyled,
   listElements,
   OnUpdateGrid,
-  RevealAnimation
-} from "./parts"
+  RevealAnimation,
+} from './parts'
 
-storiesOf("Snuggle", module)
-  .add("default", () => (
+storiesOf('Snuggle', module)
+  .add('default', () => (
     <div className="wrap">
       <Snuggle item={<div className="card" />}>{listElements()}</Snuggle>
     </div>
   ))
 
-  .add("with images", () => (
+  .add('with images', () => (
     <div className="wrap">
       <Snuggle item={<div className="card" />}>{listElements(true)}</Snuggle>
     </div>
   ))
 
-  .add("no style", () => (
+  .add('no style', () => (
     <div className="wrap">
       <Snuggle>{listElements()}</Snuggle>
     </div>
   ))
 
-  .add("on update grid", () => <OnUpdateGrid />)
+  .add('on update grid', () => <OnUpdateGrid />)
 
-storiesOf("Options", module)
+storiesOf('Options', module)
   .addDecorator(withKnobs)
 
-  .add("custom container (<ul />)", () => (
+  .add('custom container (<ul />)', () => (
     <div className="wrap">
       <Snuggle
-        container={<ul style={{ background: "#5050FF", padding: "40px" }} />}
+        container={
+          <ul
+            className="my-custom-class"
+            style={{ background: '#5050FF', padding: '40px' }}
+          />
+        }
       >
         {listElements(true)}
       </Snuggle>
     </div>
   ))
 
-  .add("custom item (<li />)", () => (
+  .add('custom item (<li />)', () => (
     <div className="wrap">
       <Snuggle
-        item={<li style={{ border: "1px solid #ddd", padding: "8px" }} />}
+        item={<li style={{ border: '1px solid #ddd', padding: '8px' }} />}
       >
         {listElements(true)}
       </Snuggle>
     </div>
   ))
 
-  .add("with Styled Components", () => (
+  .add('with Styled Components', () => (
     <div className="wrap">
       <Snuggle item={<ItemStyled />}>{listElements(true)}</Snuggle>
     </div>
   ))
 
-  .add("custom gap", () => (
+  .add('custom gap', () => (
     <div className="wrap">
       <h1>See Knobs panel</h1>
-      <Snuggle rowGap={number("rowGap", 20)} item={<div className="card" />}>
+      <Snuggle rowGap={number('rowGap', 20)} item={<div className="card" />}>
         {listElements(true)}
       </Snuggle>
     </div>
   ))
 
-  .add("custom column width", () => (
+  .add('custom column width', () => (
     <div className="wrap">
       <h1>See Knobs panel</h1>
       <Snuggle
-        columnWidth={number("columnWidth", 400)}
+        columnWidth={number('columnWidth', 400)}
         item={<div className="card" />}
       >
         {listElements(true)}
@@ -83,6 +88,6 @@ storiesOf("Options", module)
     </div>
   ))
 
-storiesOf("Third party dependencies", module).add("with scroll reveal", () => (
+storiesOf('Third party dependencies', module).add('with scroll reveal', () => (
   <RevealAnimation />
 ))
